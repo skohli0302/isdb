5e4fd97cc399a7ef38386ac2ae0ec51aaec6f834
sid <skohli0302@yahoo.com>
2013-08-30 17:40:24 +0530

    initial commit

---
+++ public_html/js/app.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function(){
+   console.log('onload function');
+});
+console.log("this app is going to be Infosprout");
+App = {};
+
---
+++ public_html/js/setup.js
@@ -0,0 +1,11 @@
+$(function() {
+    Backbone.emulateJSON = true;
+
+    // Initialize Backbone views.
+    App.chromeView = new App.ChromeView({el: $("body")});
+    App.router = new App.Router;
+
+    // Initialize the Backbone router.
+    Backbone.history.start();
+})
+
